5ccac7d03b19715f04a1f63fbf25f4f8312ebb56
caleb <jhoshmc201@gmail.com>
2023-10-25 17:59:49 -0500

    tailwin

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,30 +3,34 @@ import world from "../../assets/image/footerIcons/world.png";
 import facebook from "../../assets/image/footerIcons/facebook-svg.svg";
 import twitter from "../../assets/image/footerIcons/twitter.svg";
 import instagram from "../../assets/image/footerIcons/instagram.svg";
+
 const Footer = () => {
   return (
-    <div>
-      <div>
-        <img src={footerInc} alt="footerInc" />
-      </div>
-      <div>
+    <div class="w-screen bg-blue-500">
+      <div> ,</div>
+      <div class=" bg-fuchsia-400 mx-auto flex ">
         <div>
-          <img src={world} alt="world" />
+          <img src={footerInc} alt="footerInc" />
         </div>
-        <span>Español</span>
-      </div>
-      <div>
-        <span>$</span>
-        <span>USD</span>
-      </div>
-      <div>
-        <img src={facebook} alt="facebook" width="18px" height="18px" />
-      </div>
-      <div>
-        <img src={twitter} alt="twitter" width="18px" height="18px" />
-      </div>
-      <div>
-        <img src={instagram} alt="instagram" width="18px" height="18px" />
+        <div>
+          <div>
+            <img src={world} alt="world" />
+            <span>Español</span>
+          </div>
+          <div>
+            <span>$</span>
+            <span>USD</span>
+          </div>
+          <div>
+            <img src={facebook} alt="facebook" width="18px" height="18px" />
+          </div>
+          <div>
+            <img src={twitter} alt="twitter" width="18px" height="18px" />
+          </div>
+          <div>
+            <img src={instagram} alt="instagram" width="18px" height="18px" />
+          </div>
+        </div>
       </div>
     </div>
   );
