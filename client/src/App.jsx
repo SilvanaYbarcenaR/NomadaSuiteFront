82188368460f360f5bd99fc5303339e932dbf0ad
SilvanaYbarcenaR <silvana.ybarcena@gmail.com>
2023-10-26 00:22:28 -0500

    Footer structura and styles. Fixing Cards styles for responsive.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header/Header.jsx';
 import Home from './views/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home/>} />
         </Routes>
+        <Footer></Footer>
       </div>
   )
 }
