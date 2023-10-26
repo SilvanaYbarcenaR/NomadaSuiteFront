e9342d31b696e18ea8a624bdb3b0ed0167226267
Tardieujose <tardieujose@gmail.com>
2023-10-26 17:35:10 -0300

    Maketado header(missing some styles)v2

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -27,10 +27,6 @@ function Header() {
   };
   
 
-  const handleChange = (text) => {
-    setInputValue(text.replace(/[^A-Za-z\s]/g, ''));
-  };
-
   const onChange = (data) => {
     if (/^[a-zA-Z]*$/.test(data)){
       setValue(data);
@@ -75,6 +71,7 @@ function Header() {
           
                 <div className='flex gap-2 border border-gray-300 rounded-full py-4 px-4 shadow-md shadow-gray-400'>
                   <AutoComplete  
+                    size="large"
                     value={value}
                     options={options}
                     style={{
@@ -85,12 +82,16 @@ function Header() {
                     onChange={onChange}
                     placeholder="Ubicación"
                   />
-                  <RangePicker placeholder={['Check-in', 'Check-out']} />
+                  <RangePicker size="small" placeholder={['Check-in', 'Check-out']} />
                   <Space wrap>
-                    <InputNumber placeholder={"Huéspedes"} size="" min={1} max={10} onChange={onChange} type='number' />
+                    <InputNumber placeholder={"Huéspedes"} size="large" min={1} max={10} onChange={onChange} type='number' 
+                      style={{
+                        width: 114,
+                      }}
+                    />
                   </Space>
                   <button className='bg-primary text-white p-2 rounded-full'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.9} stroke="currentColor" className="w-4 h-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.9} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                   </button>
@@ -124,7 +125,7 @@ function Header() {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-9">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                   </svg>
-                <div className='bg-gray-500 text-white rounded-full border border-gray-500'>
+                <div className='bg-gray-500 text-white rounded-full border border-gray-500 bg-primary '>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
                     <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
                   </svg>
