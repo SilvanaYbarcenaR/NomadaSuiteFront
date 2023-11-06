31c23a4760f1152308122d79c88323df2d5e91bd
RobinsonG <hh.robinson95@hotmail.com>
2023-11-06 02:33:56 -0500

    reservation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import AccommodationDetail from './views/Detail/AccommodationDetail/AccommodationDetail';
 import Accommodation from './views/Register/Accommodation/Accommodation';
+import Cancellation from './views/Register/Cancellation/Cancellation';
 import Header from './components/Header/Header.jsx';
 import Home from './views/Home/Home';
 import Footer from './components/Footer/Footer.jsx';
+import Reservation from './views/Register/Reservation/Reservation';
 import './App.css'
-
 function App() {
 
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<AccommodationDetail />} />
         <Route path="/register-accommodation" element={<Accommodation />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/cancellation" element={<Cancellation />} />
       </Routes>
       <Footer />
     </div>
