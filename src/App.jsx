e60a643c60285e73c13089d0d26e769f7f0f67b7
Tardieujose <tardieujose@gmail.com>
2023-11-16 00:59:19 -0300

    updateando

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom/dist';
 import AccommodationDetail from './views/Detail/AccommodationDetail/AccommodationDetail';
 import Accommodation from './views/Register/Accommodation/Accommodation';
 import Cancellation from './views/Register/Reservations/Cancellation/Cancellation.jsx';
@@ -10,11 +11,40 @@ import Reservation from './views/Register/Reservations/Reservation/Reservation.j
 import AccountPage from './views/UserPanel/AccountPage.jsx';
 import './App.css'
 import Admin from './views/Admin/Admin.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUserData } from './redux/Actions/actions.js';
+
+
 function App() {
+  const dispatch = useDispatch();
+  const userLoggedInfo = useSelector((state) => state.userLogged);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (Object.keys(userLoggedInfo).length === 0 && userId) {
+      dispatch(getUserData(userId));
+    }
+  }, [userLoggedInfo]);
+
+  const isAdmin = userLoggedInfo.isAdmin;
+  console.log(isAdmin)
 
   return (
     <div>
       <Header />
+      <nav>
+        <ul>
+          <li><NavLink to="/"></NavLink></li>
+          <li><NavLink to="/detail/:id"></NavLink></li>
+          <li><NavLink to="/register-accommodation"></NavLink></li>
+          <li><NavLink to="/reservation"></NavLink></li>
+          <li><NavLink to="/cancellation"></NavLink></li>
+          <li><NavLink to="/reservation/:checkoutId"></NavLink></li>
+          <li><NavLink to="/account/:tab"></NavLink></li>
+          {isAdmin && <li><NavLink to="/admin"></NavLink></li>}
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<AccommodationDetail />} />
@@ -23,7 +53,7 @@ function App() {
         <Route path="/cancellation" element={<Cancellation />} />
         <Route path="/reservation/:checkoutId" element={<Checkout />} />
         <Route path="/account/:tab" element={<AccountPage />} />
-        <Route path="/admin" element={<Admin />} />
+        {isAdmin && <Route path="/admin" element={<Admin />} />}
       </Routes>
       <Footer />
     </div>
