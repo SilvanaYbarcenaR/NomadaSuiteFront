1b5fd3f59b6e3fb97ba66cc1706fd6d1ae86e608
SilvanaYbarcenaR <silvana.ybarcena@gmail.com>
2023-11-16 10:51:34 -0500

    Removing redirect

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ssr({
-      // Permanent redirections (HTTP status code 301)
-      redirects: {
-        '/*': '/index.html'
-      }
-    })
-  ],
+  plugins: [react()],
 })
